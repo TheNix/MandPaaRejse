2b9f5835b6e8802c372b934c9d7f5fa0165eefb1
Nikki Strømsnes <nikkistromsnes@gmail.com>
2014-09-30 15:02:39 +0200

    Isolate JS into external file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,63 +1,82 @@
+// Unminified and unconcatenated JS source for development
+
 var App = App || {};
 
-App.Namespace = (function() {
-	"use strict";
+App.Main = (function() {
+  "use strict";
 
-	var X = '',
-			Y = '',
-			Z = '',
+  var
+      // Store cached references to DOM elements
+      // that will be used over and over again
+      dom = {};
 
-			IS_TOUCH = !! ("ontouchstart" in window),
+  /**
+   * Initialize function
+   * @return {void}
+   */
+  function initialize() {
+    // Set up DOM and cache references
+    _setupDOM();
 
-			// Store cached references to DOM elements
-			// that will be used over and over again
-			dom = {};
+    // Add event listeners
+    _addEventListeners();
+  }
 
-	/**
-	 * Initialize function
-	 * @return {void}
-	 */
-	function initialize() {
-		// Set up DOM and cache references
-		_setupDOM();
+  /**
+   * Set up DOM (create?) elements and cache references for future use
+   * @return {void}
+   */
+  function _setupDOM() {
+    dom.$triggerOn  = document.querySelector('.btn--download');
+    dom.$triggerOff = document.querySelector('.modal__close');
+    dom.$modal      = document.querySelector('.modal-purchase');
+  }
 
-		// Add event listeners
-		_addEventListeners();
-	}
+  /**
+   * Attach event listeners to DOM elements
+   */
+  function _addEventListeners() {
+    dom.$triggerOn.addEventListener('click', _toggleModalOn );
+    dom.$triggerOff.addEventListener('click', _toggleModalOff );
 
-	/**
-	 * Set up DOM (create?) elements and cache references for future use
-	 * @return {void}
-	 */
-	function _setupDOM() {
-		dom.$body = $( document.body );
+    document.onkeydown = function(e) {
+      e = e || window.event;
+      if (e.keyCode == 27) { // If ESC is pressed
+        _toggleModalOff;
+      }
+    }
+  }
 
-		// Container element for this
-		dom.$wrapper = $();
-	}
+  /**
+   * Event handler for toggling modal box
+   * @param  {object} event event object
+   * @return {void}
+   */
+  function _toggleModalOn(){
+    // Make sure class can only be added once
+    if (dom.$modal.classList.contains('is-open') === false) {
+      dom.$modal.classList.add('is-open');
+    }
+  }
 
-	/**
-	 * Attach event listeners to DOM elements
-	 */
-	function _addEventListeners() {
-		dom.$body.on( 'click', _onBodyClicked );
-	}
+  function _toggleModalOff(){
+    // Class check not needed, it will always be removed
+    dom.$modal.classList.remove('is-open');
+  }
 
-	/**
-	 * Event handler for "click" inside body
-	 * @param  {object} event event object
-	 * @return {void}
-	 */
-	function _onBodyClicked( event ) {
-		alert( 'body is clicked, yo bro!' );
-	}
+  ////////////////
+  // Public API //
+  ////////////////
 
-	////////////////
-	// Public API //
-	////////////////
-
-	return {
-		initialize: initialize
-	};
+  return {
+    initialize: initialize
+  };
 
 })();
+
+document.onreadystatechange = function () {
+  // Initialize app when document is "ready"
+  if (document.readyState == "complete") {
+    App.Main.initialize();
+  }
+}
